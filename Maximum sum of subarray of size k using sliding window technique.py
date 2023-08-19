387a2dd748a2fd5a21b010ed8f7663336dfc7578
Mehul Dalal <139098439+MehulD96@users.noreply.github.com>
2023-08-19 20:14:53 +0530

    Add files via upload

---
+++ Maximum sum of subarray of size k using sliding window technique.py
@@ -0,0 +1,26 @@
+#slicising window
+#Maximum sum of subarray of size k
+arr = [1,32,8,4,86,8,7,41]
+window = int(input("enter the window size:"))
+start_size,end_size = 0,0
+sum = 0
+final_list = []
+count = -1
+for i,j in enumerate(arr):
+    if i-start_size-1 == window-1:
+        end_size = i
+        count +=1
+    sum = sum + arr[i]
+
+    if end_size-start_size-1 >= window-1:
+        start_size = count+1
+        if i == len(arr):
+            break
+        else:
+            sum = sum-arr[count]
+            final_list.append(sum)
+
+print("the original list",arr)
+print("the sum from the sliding window:",final_list)
+print("maximum sum:",max(final_list))
+
